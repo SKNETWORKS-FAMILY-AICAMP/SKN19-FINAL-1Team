5212cdb1d9a6a3185c5ba80ee219c8d1b8de58cf
ahnsui <ansui218@ewhain.net>
2026-01-12 00:34:35 +0900

    feat: 웹소켓 할루시네이션 방지

--- app/api/v1/endpoints/websocket.py
+++ app/api/v1/endpoints/websocket.py
@@ -17,12 +17,15 @@ async def websocket_endpoint(websocket: WebSocket):
     # 결과 처리 콜백
     async def on_transcription_result(text: str):
         print(f"[{session_id}] STT 결과 : {text}")
+        await websocket.send_json(text)
 
         try:
             result = await run_rag(text, config=RAGConfig(top_k=4, normalize_keywords=True))
             
             if result:
                 print(f"[{session_id}] RAG 검색 결과 : {result}")
+                # await websocket.send_json(result)  RAG 검색 결과 프론트로 전송
+
                 
         except Exception as e:
             print(f"[{session_id}] RAG 처리 중 에러 : {e}")
@@ -38,7 +41,10 @@ async def websocket_endpoint(websocket: WebSocket):
             
     except WebSocketDisconnect:
         print(f"[{session_id}] 연결 종료")
+
     except Exception as e:
         print(f"[{session_id}] 오류 발생: {e}")
+
     finally:
         whisper_service.stop()
+        print(f"[{session_id}] 연결 종료")
--- app/audio/whisper.py
+++ app/audio/whisper.py
@@ -27,6 +27,7 @@ class WhisperService:
 
     def stop(self):
         # 작업 종료 및 자원 정리
+        print("작업 스레드 종료")
         self.running = False
         self.queue.put(None)  # 종료 신호 전송
         if self.thread:
@@ -38,6 +39,11 @@ class WhisperService:
 
     def _worker(self):
         print("작업 스레드 시작")
+
+        HALLUCINATION_KEYWORDS = [
+            "시청해주셔서", "시청해 주셔서", "구독과 좋아요", 
+            "재택 플러스", "MBC", "뉴스", "투데이", 
+        ]
         
         while self.running:
             try:
@@ -54,9 +60,20 @@ class WhisperService:
                     model="whisper-1",
                     file=audio_file,
                     language="ko",
-                    prompt="이것은 카드사 상담 대화입니다. 이 상황을 고려하지만 문장을 들리는 대로 적으세요"
+                    prompt="이것은 카드사 상담 대화입니다. 침묵 구간은 무시하세요. 이 상황을 고려하지만 문장을 들리는 대로 적으세요"
                 )
                 text = transcript.text.strip()
+
+                # 할루시네이션 방지
+                if not text:
+                    self.queue.task_done()
+                    continue
+
+                if any(keyword in text for keyword in HALLUCINATION_KEYWORDS):
+                    if len(text) < 20: 
+                        print(f"[할루시네이션 감지 및 제거] : {text}")
+                        self.queue.task_done()
+                        continue
 
                 # 비동기 콜백 함수를 메인 스케줄러에 등록
                 if text and self.callback:
