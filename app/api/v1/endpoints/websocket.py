b0ebe2149d68cfe047371795b8238aec9a9648df
Windy <bsjune912@gmail.com>
2026-01-12 16:26:19 +0900

    chore: websocket.py 오탈자 수정

--- app/api/v1/endpoints/websocket.py
+++ app/api/v1/endpoints/websocket.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-from app.audio.processor import process_audio_stream
-from app.api.v1.endpoints.stt_handler import handle_stt_websocket_tasks
->>>>>>> f83d105012044fbb6c36b74e619b9fe5777b98d3
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 import asyncio
 import uuid
@@ -17,7 +12,6 @@ async def websocket_endpoint(websocket: WebSocket):
     session_id = str(uuid.uuid4())[:8]
     print(f"[{session_id}] 웹소켓 연결 완료")
 
-<<<<<<< HEAD
     whisper_service = WhisperService()
 
     # 결과 처리 콜백
@@ -45,35 +39,6 @@ async def websocket_endpoint(websocket: WebSocket):
             data = await websocket.receive_bytes()
             whisper_service.add_audio(data)
             
-=======
-    audio_queue = queue.Queue()
-    result_queue = queue.Queue()  # STT 결과를 전달하는 큐
-
-    # 해당 연결을 위한 작업 스레드 생성 및 시작
-    worker_thread = threading.Thread(
-        target=process_audio_stream,
-        args=(audio_queue, session_id, result_queue),
-        daemon=True
-    )
-
-    print(f"{session_id} 스레드 시작 시도")
-    worker_thread.start()
-    print(f"{session_id} 스레드 시작 완료")
-
-    # 연결 종료 플래그 (dict로 전달하여 참조 공유)
-    disconnected_flag = {"disconnected": False}
-
-    try:
-        # STT 웹소켓 태스크들을 병렬로 실행
-        await handle_stt_websocket_tasks(
-            websocket,
-            audio_queue,
-            result_queue,
-            session_id,
-            disconnected_flag
-        )
-    
->>>>>>> f83d105012044fbb6c36b74e619b9fe5777b98d3
     except WebSocketDisconnect:
         print(f"[{session_id}] 연결 종료")
 
@@ -81,13 +46,5 @@ async def websocket_endpoint(websocket: WebSocket):
         print(f"[{session_id}] 오류 발생: {e}")
 
     finally:
-<<<<<<< HEAD
         whisper_service.stop()
         print(f"[{session_id}] 연결 종료")
-=======
-        disconnected_flag["disconnected"] = True
-        audio_queue.put(None)
-        result_queue.put(None)
-        worker_thread.join()
-        print(f"{session_id} 리소스 정리 완료")
->>>>>>> f83d105012044fbb6c36b74e619b9fe5777b98d3
